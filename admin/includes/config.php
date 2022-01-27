117832e22d8f0abb0c579a65944f374bd05dbbe4
TeamConjurers <96539310+TeamConjurers@users.noreply.github.com>
2022-01-27 09:10:21 +0530

    First Commit
    Before deploying

---
+++ admin/includes/config.php
@@ -0,0 +1,18 @@
+<?php 
+// DB credentials.
+define('DB_HOST','34.68.31.131');
+define('DB_USER','root');
+define('DB_PASS','dbpass');
+define('DB_NAME','tms');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
+
+
---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+<div class="copyrights">
+	 <p>© 2022 Team Conjurers. All Rights Reserved |  <a href="https://smartjourney2022.herokuapp.com/
+">SMART Journey</a> </p>
+</div>	
